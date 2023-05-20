3ae20a73e7fec34c539198cd26eb61808dfc5760
savitawailthare <savitawailthare8@gmail.com>
2023-05-20 20:07:19 +0530

    my ec1

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
 provider "aws" {
-  
-}
-provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+resource "aws_instance" "web" {
+  ami           = "0b08bfc6ff7069aff"
+  instance_type = "t2.micro"
+  associate_public_ip_address ="true"
+  key_name ="key-myname"
+  security_group = ["default"]
+  ebc_block_device{
+     device_name = "/dev/sdc"
+ volume_size = 10
+ volume_type="gp2"
+  }
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "tf.demo-instance"
   }
 }
