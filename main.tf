0ae100ca14269333b147a7e33e4b37f940de3d88
savitawailthare <savitawailthare8@gmail.com>
2023-05-20 21:31:50 +0530

    my ec2

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "web" {
-  ami           = "0b08bfc6ff7069aff"
-  instance_type = "t2.micro"
-  associate_public_ip_address ="true"
-  key_name ="key-myname"
-  security_group = ["default"]
-  ebc_block_device{
-     device_name = "/dev/sdc"
- volume_size = 10
- volume_type="gp2"
+  ami                         ="ami-0b08bfc6ff7069aff"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name                    = "mumbai"
+  security_groups             = ["default"]
+  ebs_block_device {
+    device_name = "/dev/sdc"
+    volume_size = 10
+    volume_type = "gp2"
   }
 
   tags = {
